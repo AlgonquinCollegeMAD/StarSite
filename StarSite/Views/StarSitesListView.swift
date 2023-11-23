b4b86b00aef607ad2a1b8e03e799dd31b4bc67d3
Vladimir Cezar <vladimircezar@icloud.com>
2023-11-22 21:38:44 -0500

    Add webview and create link for safari

--- StarSite/Views/StarSitesListView.swift
+++ StarSite/Views/StarSitesListView.swift
@@ -6,13 +6,23 @@ struct StarSitesListView: View {
     NavigationView {
       List {
         ForEach(model.starSites, id: \.id) { model in
-          NavigationLink(model.name) {
-            Text(model.name)
+          NavigationLink {
+            VStack {
+              Link("Open this link in the browser", destination: model.url)
+              WebView(url: model.url)
+              Spacer()
+            }
+          } label: {
+            HStack {
+              Text(model.name)
+              Spacer()
+            }
           }
         }
       }
       .navigationTitle("Star Sites")
     }
+    
   }
 }
 
--- StarSite/Views/WebView.swift
+++ StarSite/Views/WebView.swift
@@ -1,1 +1,15 @@
 import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+  let webView = WKWebView(frame: .zero)
+  var url: URL
+  
+  func makeUIView(context: Context) -> WKWebView {
+    return webView
+  }
+  
+  func updateUIView(_ uiView: WKWebView, context: Context) {
+    webView.load(URLRequest(url: url))
+  }
+}
